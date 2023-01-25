2bd78d91765eb782bb2cbea5eec2af52f9f9ce15
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-25 13:01:51 -0500

    note model completed

---
+++ app/models/note.py
@@ -0,0 +1,19 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Note(db.Model):
+    __tablename__="note"
+
+     if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    notebook_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notebooks.id')), nullable=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(2000), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+
+    # author = db.relationship('User')
