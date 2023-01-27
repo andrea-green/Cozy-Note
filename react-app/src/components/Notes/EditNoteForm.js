78610d803d32e2a6cb305139f066dab16e9a0c3a
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-27 00:28:08 -0500

    edit note form jsx added

--- react-app/src/components/Notes/EditNoteForm.js
+++ react-app/src/components/Notes/EditNoteForm.js
@@ -5,7 +5,7 @@ import { editNoteThunk } from '../../store/notes';
 import './DeleteNoteForm.css'
 
 
-export default  function EditNoteForm(){
+export default function EditNoteForm() {
     const dispatch = useDispatch();
     const history = useHistory();
     const { noteId } = useParams();
@@ -18,16 +18,16 @@ export default  function EditNoteForm(){
     const updateTitle = (e) => setTitle(e.target.value);
     const updateContent = (e) => setContent(e.target.value);
 
-    const note = useSelector(state => console.log('state',state));
+    const myNote = useSelector(state => console.log('state', state));
 
     useEffect(() => {
-        const errors=[];
+        const errors = [];
 
         if (title.length < 1) errors.push('Title must be at least 1 characters long');
         if (content.length < 0) errors.push('Content may not be empty');
 
         setErrors(errors);
-    },[title, content])
+    }, [title, content])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -47,5 +47,36 @@ export default  function EditNoteForm(){
 
 
 
-    return null;
+    return <>
+        <div className='edit-note-form'>
+            <h1>Edit your note</h1>
+        </div>
+        <section className='edit-note-container'>
+            <div className='edit-note-errors'>
+                <ul> {errors.map((error) => (
+                    <li key={error}>{error}</li>
+                ))}
+                </ul>
+            </div>
+
+            <form className='edit-note-form-body' onSubmit={handleSubmit}>
+                <input className='edit=note-form-input'
+                    type="text"
+                    placeholder={myNote.title}
+                    required
+                    value={title}
+                    onChange={updateTitle}
+                />
+
+                <input className='edit-note-form-input'
+                    type="text"
+                    placeholder={myNote.content}
+                    required
+                    value={content}
+                    onChange={setName}
+                />
+            </form>
+
+        </section>
+    </>;
 }
