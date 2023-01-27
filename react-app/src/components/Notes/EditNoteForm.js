181f0b7498e6bc4de74ccf7a300b821fadaccb09
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-01-26 21:17:55 -0500

    edit note form done

---
+++ react-app/src/components/Notes/EditNoteForm.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { editNoteThunk } from '../../store/notes';
+
+export default  function EditNoteForm(){
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { noteId } = useParams();
+
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    const updateTitle = (e) => setTitle(e.target.value);
+    const updateContent = (e) => setContent(e.target.value);
+
+    const note = useSelector(state => console.log('state',state));
+
+    useEffect(() => {
+        const errors=[];
+
+        if (title.length < 1) errors.push('Title must be at least 1 characters long');
+        if (content.length < 0) errors.push('Content may not be empty');
+
+        setErrors(errors);
+    },[title, content])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const payload = {
+            title,
+            content
+        }
+
+        return dispatch(editNoteThunk(payload))
+            .then(() => history.push('/notes'))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            });
+    }
+
+
+
+    return null;
+}
