80633e89d2e822aa5473b2e6db6914555c48532d
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-04 19:06:50 -0500

    homepage notes container cards styled

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import logoutButton from '../images/logout-button.png'
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
   };
-
-  return <button className='logout-button' onClick={onLogout}>Logout</button>;
+  return (
+    <div>
+      <button className='logout-button' onClick={onLogout}>LOGOUT</button>
+    </div>
+  )
+  // return <button className='logout-button' onClick={onLogout}>Logout</button>;
 };
 
 export default LogoutButton;
