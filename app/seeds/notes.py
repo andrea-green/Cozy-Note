26cb5b53c662c5ccbb47686d1e7269674343f70b
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-06 01:54:43 -0700

    added seeders to notes and notebooks

--- app/seeds/notes.py
+++ app/seeds/notes.py
@@ -20,9 +20,64 @@ def seed_notes():
         notebook_id=2,
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
     )
+    note_4= Note(
+        title="note 4",
+        author_id=1,
+        notebook_id=2,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_5= Note(
+        title="note 5",
+        author_id=1,
+        notebook_id=3,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_6= Note(
+        title="note 6",
+        author_id=1,
+        notebook_id=3,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_7= Note(
+        title="note 7",
+        author_id=2,
+        notebook_id=1,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_8= Note(
+        title="note 8",
+        author_id=1,
+        notebook_id=1,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_9= Note(
+        title="note 9",
+        author_id=1,
+        notebook_id=5,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_9= Note(
+        title="note 10",
+        author_id=1,
+        notebook_id=1,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
+    note_10= Note(
+        title="note 10",
+        author_id=1,
+        notebook_id=1,
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo augue orci, sed luctus erat ornare sit amet."
+    )
     db.session.add(note_1)
     db.session.add(note_2)
     db.session.add(note_3)
+    db.session.add(note_4)
+    db.session.add(note_5)
+    db.session.add(note_6)
+    db.session.add(note_7)
+    db.session.add(note_8)
+    db.session.add(note_9)
+    db.session.add(note_10)
 
     db.session.commit()
 
