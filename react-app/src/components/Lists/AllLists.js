e67e5e6106d54b5fb0c8d3d0ab0dca1b1d529ca0
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-21 15:22:16 -0700

    create list form done

--- react-app/src/components/Lists/AllLists.js
+++ react-app/src/components/Lists/AllLists.js
@@ -2,6 +2,10 @@ import React, {useState,useEffect} from "react";
 import {useSelector, useDispatch} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import { getAllListsThunk } from "../../store/list";
+import CreateListForm from "./CreateListForm";
+//set up icon  modal for create list form
+//make an index to hold edit/delete/ single list components.
+
 
 export default function AllLists(){
     const dispatch = useDispatch();
@@ -20,6 +24,9 @@ export default function AllLists(){
             <div className='my-to-do-header'>
                 <h1>My Lists </h1>
             </div>
+            <div className='create-list'>
+                <CreateListForm />
+            </div>
             <div className='list-content'>
                 {myListsArr.map((list)=> (
                     <div className='list-card' key={list.id}
@@ -32,7 +39,6 @@ export default function AllLists(){
                     <div>
                         <span>{list.created_at}</span>
                     </div>
-
                     </div>
                 ))}
 
---
+++ react-app/src/components/Lists/CreateListForm.js
@@ -0,0 +1,58 @@
+import React, {useEffect,useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { addListThunk } from '../../store/list';
+
+export default function CreateListForm(){
+    const dispatch = useDispatch();
+
+    const [title,setTitle] = useState('');
+    const enterTitle = (e) => setTitle(e.target.value);
+
+    const [errors,setErrors] = useState([]);
+    const {closeModal} = useModal();
+
+    useEffect(()=>{
+        const errors=[];
+        if(title.length < 1) errors.push('Title must be at least 1 character long');
+        setErrors(errors);
+    },[title])
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+        const payload = {
+            title
+        }
+
+        await dispatch(addListThunk(payload))
+        .then(()=>{
+            closeModal()
+        })
+    };
+    return (
+        <div className='create-list-main'>
+            <div className='form-header'>
+                <h1>New List</h1>
+            </div>
+
+            <section className='form-container'>
+                <ul>{errors.map((error)=> (
+                    <li key={error}>{error}</li>
+                ))}</ul>
+                <form className='form-body' onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input className='list-form-input'
+                        type="title"
+                        required
+                        value={title}
+                        onChange={enterTitle}
+                    />
+                    <button
+                    classNamae='button form-button'
+                    type='submit'
+                    >Submit</button>
+                </form>
+            </section>
+        </div>
+    )
+
+}
