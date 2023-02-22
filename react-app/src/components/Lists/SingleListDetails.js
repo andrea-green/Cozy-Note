0fea06ff733f0840954c9970a4ca87aa72665965
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-21 17:45:20 -0700

    single list details component done

--- react-app/src/components/Lists/SingleListDetails.js
+++ react-app/src/components/Lists/SingleListDetails.js
@@ -1,7 +1,31 @@
-import React from 'react'; 
+import React, { useEffect } from "react";
+// import all the tasks belonging to the specific list.
+import { useSelector,useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getSingleListThunk } from "../../store/list";
+//also need to import thunk to grab all of the tasks of a list.
 
 export default function SingleListDetails(){
+    const myList = useSelector(state=>state.lists.singleList)
+    const dispatch = useDispatch();
+    const {listId} = useParams();
 
 
-    return null;
+    useEffect(()=>{
+        dispatch(getSingleListThunk(listId))
+    },[dispatch,listId]);
+
+    // have use effect for getting a taks thunk? do i even need this? questionable.
+
+
+    return (
+        <div className='single-list-details-main'>
+            <div className='single-list-details-header'>
+                <h1>{myList.title}</h1>
+            </div>
+            {/* <div className='single-list-tasks'>
+                will import the tasks belonging to a single note component here.
+            </div> */}
+        </div>
+    );
 }
