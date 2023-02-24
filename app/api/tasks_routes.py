a3dcce211429aa7ea0300f7fbbbb27ace6b6d95a
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-24 10:46:34 -0700

    delete task route done;

--- app/api/tasks_routes.py
+++ app/api/tasks_routes.py
@@ -7,6 +7,8 @@ from .validation_to_error_formatter import validation_errors_to_error_messages
 tasks_routes = Blueprint('tasks', __name__)
 
 # get all tasks of current user
+
+
 @tasks_routes.route('/')
 @login_required
 def get_user_tasks():
@@ -21,6 +23,8 @@ def get_user_tasks():
     return jsonify({"Tasks": tasks_dict})
 
 # get single task by id
+
+
 @tasks_routes.route('/<int:task_id', methods=['GET'])
 @login_required
 def get_single_task(task_id):
@@ -34,6 +38,8 @@ def get_single_task(task_id):
     return jsonify({"Task": task_dict})
 
 # create a new task
+
+
 @tasks_routes.route('/', methods=["POST"])
 @login_required
 def create_task():
@@ -48,19 +54,19 @@ def create_task():
         db.session.commit()
 
         return task.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)},400
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 
 # update task route
-@tasks_routes.route('/<int:task_id',methods=['PUT'])
+@tasks_routes.route('/<int:task_id', methods=['PUT'])
 @login_required
 def update_task(task_id):
-    task=Task.query.get(task_id)
+    task = Task.query.get(task_id)
     if task is None:
         return jsonify({'error': "Task not found"}), 404
     if task.owner_id != current_user.id:
         return jsonify({'error': "Unauthorized"}), 401
-    listId=request.json['listId']
+    listId = request.json['listId']
     list = List.query.get(listId)
 
     form = TaskForm()
@@ -69,9 +75,24 @@ def update_task(task_id):
         if listId != None:
             task.list_id = listId
             task.list = list
-        task.content=form.data['content']
+        task.content = form.data['content']
         db.session.commit()
-        task_dict=task.to_dict()
-        task_dict['list']=list.to_dict()
+        task_dict = task.to_dict()
+        task_dict['list'] = list.to_dict()
         return task_dict
-    return {'errors': validation_errors_to_error_messages(form.errors)},400
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+# delete task
+@tasks_routes.route('/<int:task_id', methods=['PUT'])
+@login_required
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify({'error': "Task not found"}), 404
+    if task.owner_id != current_user.id:
+        return jsonify({'error': "Unauthorized"}), 401
+
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({"message":"Task successfully deleted"}),200 
