7164629105e4046b196fe5a40bc31a751a3c0e86
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-28 21:57:29 -0600

    create task store done

--- react-app/src/store/task.js
+++ react-app/src/store/task.js
@@ -1,6 +1,7 @@
 
 const GET_ALL_TASKS='tasks/GET_ALL_TASKS';
 const GET_TASK = 'tasks/GET_TASK';
+const CREATE_TASK = 'tasks/CREATE';
 
 
 
@@ -15,6 +16,11 @@ const getAllTasksAc=(tasks) =>({
 const getTaskAc = (task) => ({
     type:GET_TASK,
     payload:task
+});
+
+const createTaskAc = (task) => ({
+    type:CREATE_TASK,
+    payload:task
 })
 
 
@@ -22,7 +28,7 @@ const getTaskAc = (task) => ({
 // thunks
 
 export const getAllTasksThunk = () => async(dispatch) =>{
-    const response = await fetch(`/api/lists`,{
+    const response = await fetch(`/api/tasks`,{
         headers:{
             'Content-Type':'application/json'
         }
@@ -43,7 +49,7 @@ export const getAllTasksThunk = () => async(dispatch) =>{
 
 
 export const getTaskThunk = (taskId) => async(dispatch) =>{
-    const response = await fetch(`api/lists/${taskId}`,{
+    const response = await fetch(`api/tasks/${taskId}`,{
         headers:{
             'Content-Type':'application/json'
         }
@@ -59,6 +65,29 @@ export const getTaskThunk = (taskId) => async(dispatch) =>{
         }
     }else {
         return ['An error occurred.Please try again.']
+    }
+}
+
+export const createTaskThunk = (task) => async(dispatch)=>{
+    const response = await fetch (`api/tasks`, {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    });
+
+    if (response.ok){
+        const data = await response.json();
+        dispatch(createTaskAc(data));
+        return data
+    } else if (response.status < 500 ){
+        const data = await response.json();
+        if(data.errors){
+            return data.errors;
+        }
+    } else {
+        return response
     }
 }
 
@@ -95,6 +124,17 @@ export default function taskReducer(){
                 ...state,
                 singleTask:task
             };
+        case CREATE_TASK:
+            const newTask = action.payload;
+            const newTaskState = {
+                allTasks:{
+                    byId:{...state.allTasks.byId},
+                    allIds:[...state.allTasks.allIds,newTask.id],
+                },
+                singleTask:newTask
+            };
+            newTaskState.allTasks.byId[newTask.id]=newTask;
+            return newTaskState; 
         default:
             return state;
     }
