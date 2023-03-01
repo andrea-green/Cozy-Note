03fb448ab28650154a6e702ef3062c26c8f45a05
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-01 11:39:06 -0600

    delete task form done

--- react-app/src/components/Tasks/AllTasks.js
+++ react-app/src/components/Tasks/AllTasks.js
@@ -1,11 +1,29 @@
-
-
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import {getTaskThunk, getAllTasksThunk} from '../../store/task'
+import OpenModalButton from '../OpenModalButton';
+import {useModal} from '../../context/Modal'; 
 
 
 export default function AllTasks(){
+    const dispatch = useDispatch();
+    const history = useDispatch
+
+    const handleSubmit = async(taskId) => {
+        await dispatch(getTaskThunk(taskId))
+        history.push(`/tasks/${taskId}`)
+    }
+    useEffect(()=>{
+        dispatch(getAllTasksThunk())
+    },[dispatch])
 
 
+    const myTasks = useSelector((state) => state.tasks.allTasks.byId);
+    const tasksArr = Object.values(myTasks);
 
 
-    return null; 
+    useEffect(()=>{},[myTasks])
+
+    return null;
 }
---
+++ react-app/src/components/Tasks/DeleteTaskForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { useModal } from '../../context/Modal';
+import { deleteTaskThunk } from '../../store/task';
+
+
+export default function DeleteTaskForm(){
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+
+    const myTask = useSelector(state => state.tasks.singleTask);
+    const [errors,setErrors] = useState([]);
+    const [boolean, setBoolean] = useState(false);
+
+
+    const trueBoolean = (e) => setBoolean(true);
+    const falseBoolean = (e) => setBoolean(false);
+    const {closeModal} = useModal()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await dispatch(deleteTaskThunk(myTask.id))
+        history.push('/tasks')
+        closeModal();
+    }
+
+    return (
+        <div>
+            <div className='form-header'>
+                <h1>Are you sure you want to delete this task?</h1>
+            </div>
+            <section className='form-body-container'>
+                <div className='delete-errors'>
+                    <ul>{errors.map((error) => (
+                        <li key={error}>{error}</li>
+                    ))}
+                    </ul>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        No
+                        <input className='button'
+                            type="radio"
+                            required
+                            checked={boolean ? false : true}
+                            onChange={falseBoolean}
+                        />
+                    </label>
+                    <label>
+                        Yes
+                        <input className='button'
+                            type="radio"
+                            required
+                            checked={boolean}
+                            onChange={trueBoolean}
+                        />
+                    </label>
+                    <button
+                        className='button form-button'
+                        type="submit"
+                        disabled={!boolean}
+                    >Confirm</button>
+                </form>
+            </section>
+
+
+        </div>
+
+    );
+}
