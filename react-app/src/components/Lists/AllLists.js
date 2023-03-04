3cc72cfebf88ac073054a50f3a4c2a18b1818ecd
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-04 15:29:30 -0600

    list carousel set up

--- react-app/src/components/Lists/AllLists.js
+++ react-app/src/components/Lists/AllLists.js
@@ -1,50 +1,70 @@
-import React, {useEffect} from "react";
-import {useSelector, useDispatch} from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { getAllListsThunk, getSingleListThunk } from "../../store/list";
 import CreateListForm from "./CreateListForm";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 //set up icon  modal for create list form
 
 
 
-export default function AllLists(){
+export default function AllLists() {
     const dispatch = useDispatch();
-    const history= useHistory();
-    const myLists = useSelector((state)=>state.lists.allLists.byId);
+    const history = useHistory();
+    const myLists = useSelector((state) => state.lists.allLists.byId);
     const myListsArr = Object.values(myLists);
 
-    useEffect(()=>{
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    };
+
+
+
+    useEffect(() => {
         dispatch(getAllListsThunk())
-    },[myLists])
+    }, [myLists])
 
 
-    const handleSubmit=(e,listId) =>{
+    const handleSubmit = (e, listId) => {
         e.preventDefault();
         dispatch(getSingleListThunk(listId))
-            .then(()=>{history.push(`/lists/${listId}`)})
+            .then(() => { history.push(`/lists/${listId}`) })
     }
+
 
     return (
         <div className='all-lists-main-container'>
             <div className='my-to-do-header'>
-                <h1>My Lists </h1>
+                <h1>My Lists</h1>
             </div>
-    
-            <div className='list-content'>
-                {myListsArr.map((list)=> (
-                    <div className='list-card' key={list.id}
-                        onClick={()=>handleSubmit(list.id)}
-                        style={{cursor:'pointer'}}
-                    >
-                    <div>
-                        <h3>{list.title}</h3>
-                    </div>
-                    <div>
-                        <span>{list.created_at}</span>
-                    </div>
-                    </div>
-                ))}
 
+            <div className='list-content'>
+                <Slider {...settings}>
+                    {myListsArr.map((list) => (
+                        <div
+                            className='list-card'
+                            key={list.id}
+                            onClick={() => handleSubmit(list.id)}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <div>
+                                <h3>{list.title}</h3>
+                            </div>
+                            <div>
+                                <span>{list.created_at}</span>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
             </div>
         </div>
     );
