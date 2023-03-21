574a76c25de3fc753d4406a59a234ad26a5fe644
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-21 14:58:08 -0500

    note container styling

--- react-app/src/components/Notes/AllNotes.js
+++ react-app/src/components/Notes/AllNotes.js
@@ -6,7 +6,7 @@ import notecard from '../images/indiv-note.png'
 import IconModal from '../IconModal/IconModal'
 import CreateNoteForm from './CreateNoteForm'
 import headerPic from '../images/my-notes-header.png'
-
+import notesFiller from '../images/notes-filler.gif'
 
 export default function AllNotes() {
     const dispatch = useDispatch();
@@ -32,7 +32,7 @@ export default function AllNotes() {
     }, [myNotes])
 
     return (
-        <div style={{ border: '1px solid black', overflow: 'auto', background: 'white', height: '50vh',borderRadius:'10px',marginLeft:'1rem',width:'45vw',marginRight:'1rem',marginTop:"0" }}>
+        <div style={{ border: '1px solid black', overflow: 'auto', background: 'white', height: '50vh', borderRadius: '10px', marginLeft: '1rem', width: '45vw', marginRight: '1rem', marginTop: "0" }}>
             <div className='my-notes-header'>
                 <img src={headerPic} alt='header' style={{ height: '300px', marginTop: '30px' }} />
                 <div className='create-new-note' style={{ padding: '35px' }}>
@@ -42,22 +42,33 @@ export default function AllNotes() {
                     />
                 </div>
             </div>
-            <div className="home-notes-list">
-                {myNotesArr.map((note) => (
-                    <div className='card-container' key={note.id}
-                        onClick={() => { handleSubmit(note.id) }}
-                        style={{ cursor: 'pointer' }}
-                    >
-                        <div>
-                            <h3>{note.title}</h3>
-                        </div>
-                        <div>
-                            <span>{note.updated_at}</span>
-                        </div>
-                    </div>
+            {!myNotesArr.length
+                ? (
+                    <div>
+                        <img src={notesFiller} alt='img' style={{
+                            width: 'auto',
+                            height: '12rem',
+                            paddingLeft: '10rem'}}/>
+                    </ div>)
+                : (
+                    <div className="home-notes-list">
+                        {myNotesArr.map((note) => (
+                            <div className='card-container' key={note.id}
+                                onClick={() => { handleSubmit(note.id) }}
+                                style={{ cursor: 'pointer' }}
+                            >
+                                <div>
+                                    <h3>{note.title}</h3>
+                                </div>
+                                <div>
+                                    <span>{note.updated_at}</span>
+                                </div>
+                            </div>
 
-                ))}
-            </div>
+                        ))}
+                    </div>)
+            }
+
 
         </div>
     );
