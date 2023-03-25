eafe9782c7e58378fd8162c35285881f871780e4
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-25 13:05:48 -0500

    update

---
+++ react-app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from './new-graphics/cozy-note2-logo.png';
+import Login from './new-graphics/login-head.png';
+import Logout from './new-graphics/logout.png';
+import SignUp from './new-graphics/signup-head.png';
+
+
+
+
+
+const Header = () => {
+
+    return (
+        <div className="container">
+            <p> Hello World </p>
+        </div>
+    )
+}
+
+
+export default Header;
