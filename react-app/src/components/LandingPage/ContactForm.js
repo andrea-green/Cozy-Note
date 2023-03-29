c5749cb4e8cf4e6fd6be88471ec9f8f39c2a9e17
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-29 11:42:46 -0500

    contact form basic styling

--- react-app/src/components/LandingPage/ContactForm.js
+++ react-app/src/components/LandingPage/ContactForm.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import { useModal } from '../../context/Modal';
+import gif6 from '../graphics/gif6.gif';
 
 export default function ContactForm({ onFormSubmit }) {
     const handleSubmit = (event) => {
-      event.preventDefault();
-      
-      const formData = new FormData(event.target);
-      const name = formData.get('name');
-      const message = formData.get('message');
-      const emailBody = `Name: ${name}\n\nMessage: ${message}`;
-      window.open(`mailto:duong.andrea12@gmail.com?subject=Contact Form Submission&body=${emailBody}`);
-      onFormSubmit();
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const name = formData.get('name');
+        const message = formData.get('message');
+        const emailBody = `Name: ${name}\n\nMessage: ${message}`;
+        window.open(`mailto:duong.andrea12@gmail.com?subject=Contact Form Submission&body=${emailBody}`);
+        onFormSubmit();
     };
 
     return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
+        <div className='contact-main'>
+            <div className='left-side'>
+                <img src={gif6} alt='gif6' />
+            </div>
+            <div className='right-side'>
+                <h1>Contact Us </h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" required />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <textarea id="message" name="message" required></textarea>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" required></textarea>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
     );
-  }
+}
