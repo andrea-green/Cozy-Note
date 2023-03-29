924b731ab154203da7b7bb76fc78397ff36aa857
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-28 20:09:48 -0500

    s2 styled and added

--- react-app/src/components/LandingPage/Section1.js
+++ react-app/src/components/LandingPage/Section1.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import gif3 from '../graphics/gif3.gif'
+import gif3 from '../graphics/gif3.gif';
+import gif1 from '../graphics/gif1.gif';
 
 
 
@@ -18,11 +19,28 @@ export default function Section1() {
                         helps you stay organized in a
                         way that
                         <br />
-                        works for you </p>
+                        works for you. </p>
                 </div>
                 <div className='s1-right'>
                     <img src={gif3} alt='gif' />
                 </div>
+            </div>
+            <div className='s1-main'>
+                <div className='s2-left'>
+                    <img src={gif1} alt='gif' />
+                </div>
+                <div className='s2-right'>
+                    <h1>Find your productivity happy place!</h1>
+                    <p>See what's possible with CozyNote today.
+                        <br />
+                        <a href="/login">Login</a>
+                        or
+                        <a href="/sign-up">sign up</a>
+                        to get started.
+                    </p>
+                </div>
+
+
             </div>
         </>
     )
