f6c254b7c24d98e3aa572433a14b468efcb10a25
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-04-02 11:03:25 -0500

    logout routing fixed

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
 import logoutButton from '../graphics/logout.png'
 import "../HomePage.css"
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/')
   };
   return (
     <div>
