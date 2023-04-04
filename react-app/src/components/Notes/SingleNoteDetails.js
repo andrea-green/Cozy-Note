40847d9195253a5d866261accf151f81cae2e4d0
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-04-04 14:55:11 -0500

    trash can and notebook dropdown rendered

--- react-app/src/components/Notes/SingleNoteDetails.js
+++ react-app/src/components/Notes/SingleNoteDetails.js
@@ -8,6 +8,7 @@ import { useParams } from 'react-router-dom'
 import NotebookDropDown from './NotebooksDropDown'
 import EditNoteTitle from './EditNoteTitle'
 import EditNoteContent from './EditNoteContent'
+import OpenModalButton from '../OpenModalButton'
 // import '../HomePage.css'
 
 
@@ -33,8 +34,22 @@ export default function SingleNoteDetails() {
     return (
         // <h1>welcome to clown town </h1>
         <div className='sn-main'>
-            <NotebookDropDown />
-            
+            <div className='note-header'>
+                <h1 style={{ color: 'black', fontSize: '20px', marginBottom: '0' }}>Notebook</h1>
+                {myNote.notebook &&
+                    <h1 className='notebook-name'>{myNote.notebook.name} </h1>
+                }
+                <div className='drop-down'>
+                    <NotebookDropDown />
+                </div>
+            </div>
+            <div className='delete-edit'>
+                <OpenModalButton
+                    modalComponent={<DeleteNoteForm />}
+                    faIcon={<i className="fa-solid fa-trash fa-2xl" style={{ color: "#cacafa", cursor: 'pointer' }} />}
+                />
+
+            </div>
         </div>
         // <div>
         //     <div className='note-header'>
