5e1fe8186bcf01381f078efc87f392c14794243a
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-04-04 20:24:52 -0500

    notes list container rendered

--- react-app/src/components/Notes/notes-list.js
+++ react-app/src/components/Notes/notes-list.js
@@ -1,7 +1,18 @@
-
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { useHistory } from "react-router-dom"
+import { getNoteThunk, getAllNotesThunk } from '../../store/note';
+import OpenModalButton from '../OpenModalButton';
+import CreateNoteForm from './CreateNoteForm'
+import { useModal } from '../../context/Modal';
+import notesHeader from '../graphics/header4.png';
+import './index.css'
 
 export default function NotesList() {
     return (
-        <h1>WELCOME TO THE CIRCUS</h1>
+        <div className='nl-main'>
+            <h1>WELCOME TO THE CIRCUS</h1>
+
+        </div>
     )
 }
