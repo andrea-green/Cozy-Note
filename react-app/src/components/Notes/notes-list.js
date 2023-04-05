8a6caa3138f7e7f0cf16d680fc3d6d5193b6973f
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-04-04 20:33:47 -0500

    create note button added to notes list section

--- react-app/src/components/Notes/notes-list.js
+++ react-app/src/components/Notes/notes-list.js
@@ -7,11 +7,21 @@ import CreateNoteForm from './CreateNoteForm'
 import { useModal } from '../../context/Modal';
 import notesHeader from '../graphics/header4.png';
 import './index.css'
+import CreateNotebookForm from '../Notebooks/CreateNotebookForm';
 
 export default function NotesList() {
     return (
         <div className='nl-main'>
-            <h1>WELCOME TO THE CIRCUS</h1>
+            {/* <h1>WELCOME TO THE CIRCUS</h1> */}
+            <div className='nl-header'>
+                <img src={notesHeader} alt='nl-header'/>
+                <div className='nl-create-button'>
+                    <OpenModalButton
+                        modalComponent={<CreateNotebookForm />}
+                        buttonText='Create notebook'
+                    />
+                </div>
+            </div>
 
         </div>
     )
